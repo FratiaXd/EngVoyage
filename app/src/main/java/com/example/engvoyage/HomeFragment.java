2bde420d59617244a9a63292af32931ce2bc67f0
FratiaXd <welcomcherry@gmail.com>
2024-01-23 21:39:28 +0000

    setting up course boxes

--- app/src/main/java/com/example/engvoyage/HomeFragment.java
+++ app/src/main/java/com/example/engvoyage/HomeFragment.java
@@ -3,6 +3,7 @@ package com.example.engvoyage;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
