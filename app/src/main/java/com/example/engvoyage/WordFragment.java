003990591ab55ecf094e1a6ccb2a8bf99b4c790f
FratiaXd <welcomcherry@gmail.com>
2024-01-31 19:26:30 +0000

    added and styled vocabulary builder pages

---
+++ app/src/main/java/com/example/engvoyage/WordFragment.java
@@ -0,0 +1,63 @@
+package com.example.engvoyage;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+public class WordFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public WordFragment() {
+        // Required empty public constructor
+    }
+
+    public static WordFragment newInstance(String param1, String param2) {
+        WordFragment fragment = new WordFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_word, container, false);
+        returnToBuilder(view);
+        return view;
+    }
+
+    private void returnToBuilder(View view) {
+        ImageButton back = (ImageButton) view.findViewById(R.id.goBackToBuilder);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fr = getParentFragmentManager().beginTransaction();
+                fr.replace(R.id.frame_layout, new BuilderFragment());
+                fr.commit();
+            }
+        });
+    }
+}
